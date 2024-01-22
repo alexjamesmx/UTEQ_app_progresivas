02e51d82f07f124ddd165f6592d888ccb999c6c9
Alex Santiago <sg.alex32@gmail.com>
2024-01-22 17:34:07 -0600

    fetch2

--- fetch1.js
+++ fetch1.js
@@ -1,4 +1,7 @@
-fetch("https://fakestoreapi.com/products")
+fetch("https://fakestoreapi.com/products/1")
   .then((response) => response.json())
-  .then((data) => console.log(data))
+  .then((data) => {
+    console.log(data)
+    console.log(data.price)
+  })
   .catch((error) => console.log(error))
