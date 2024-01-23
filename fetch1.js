f1ff46a602b80ef4adb55389cc94dc66b986582e
Alex Santiago <sg.alex32@gmail.com>
2024-01-22 19:50:44 -0600

    promesas2

--- fetch1.js
+++ fetch1.js
@@ -1,20 +1,12 @@
-//uso de promesas
-// las promesas son objetos que representan la terminacion o el fracaso eventual de una operacion asincrona
-
-let promesaExitosa = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("La operacion fue exitosa")
-  }, 3000)
+let promesaConError = new Promise((resolve, reject) => {
+  reject("Algo salió mal")
 })
 
-//manejo de promesas
-//then() se ejecuta cuando la promesa se resuelve exitosamente
-//catch() se ejecuta cuando la promesa se rechaza
-
-promesaExitosa
+//Manejar la promesa con error
+promesaConError
   .then((mensaje) => {
     console.log(mensaje)
   })
   .catch((error) => {
-    console.error(error)
+    console.log(error)
   })
