f96ed73e1e44cce438147df44d4277cde198b322
Alex Santiago <sg.alex32@gmail.com>
2024-01-24 17:24:08 -0600

    promesas7

--- fetch1.js
+++ fetch1.js
@@ -1,32 +1,17 @@
-const sumar = (n1, n2) => {
-  return new Promise((resolve, reject) => {
-    resolve(n1 + n2)
+fetch("https://fakestoreapi.com/products/1").then((resp) => {
+  resp
+    .clone()
+    .json()
+    .then((data) => {
+      console.log(data)
+    })
+  resp
+    .clone()
+    .json()
+    .then((data) => {
+      console.log(data)
+    })
+  resp.json().then((data) => {
+    console.log(data)
   })
-}
-
-const resta = (n1, n2) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(function () {
-      resolve(n1 - n2)
-    }, 1200)
-  })
-}
-
-const mensaje = (mensaje) => {
-  return new Promise((resolve, reject) => {
-    resolve(mensaje)
-  })
-}
-
-// error
-// const mensaje = (mensaje) => {
-//     return new Promise((resolve, reject) => {
-//         reject('Error en la promesa');
-//     });
-// }
-
-Promise.all([resta(8, 8), sumar(5, 5), mensaje("Hola miss")])
-  .then((respuestas) => {
-    console.log(respuestas)
-  })
-  .catch(console.log)
+})
