323fd9de1f0c3de536fa7115ec2323f51e5b025f
Alex Santiago <sg.alex32@gmail.com>
2024-01-24 17:59:21 -0600

    promesas9

--- fetch1.js
+++ fetch1.js
@@ -1,8 +1,24 @@
-fetch("not-found.html")
-  .then((resp) => resp.text())
-  .then((html) => {
-    document.querySelector("body").innerHTML = html
-  })
-  .catch((err) => {
-    console.log("error: ", err)
+//ejercicio
+// crea una solicitud de la api de fake y pinta los datos en el archivo
+// index.html
+// Utiliza promseas para en caso de que la solicitud no sea exitosa devuelva el contenido
+// del archivo not=found.html con el mensaje de que no se ha podido completar la solicitud
+
+fetch("https://fakestoreapi.com/products")
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data)
+    let html = ""
+    data.forEach((element) => {
+      html += `
+        <div>
+        <img src="${element.image}">
+          <h5>${element.title}</h5>
+          <p>${element.description}</p>
+      </div>
+        `
+    })
+    div = document.createElement("div")
+    div.innerHTML = html
+    document.body.appendChild(div)
   })
