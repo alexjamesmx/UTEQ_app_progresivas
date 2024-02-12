414f9f1f172028791e6f42169d2191bea1e6df6d
Alex Santiago <sg.alex32@gmail.com>
2024-02-12 17:43:09 -0600

    app-shell

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,3 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then((reg) => {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          reg.showNotification("Hello from SW")
-        }
-      })
-    })
-    .catch((err) => console.log(err))
-  // .then((reg) => {
-  //   setTimeout(() => {
-  //     reg.sync.register("Enviar-fotos")
-  //     console.log("Se enviaron fotos al server")
-  //   }, 5000)
-  // })
-  // .catch((err) => console.error("Error al registrar el service worker", err))
-  // .then((req) => console.log("Service Worker Registered!"))
-  // .catch((err) => console.log("Service Worker Failed to Register!"))
+  navigator.serviceWorker.register("/sw.js")
 }
-
-// fetch("https://fakestoreapi.com/products/1")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data))
--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,17 @@
-self.addEventListener("install", function (event) {
-  console.log("Service worker installed")
-  const instalacion = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Instalaciones terminadas")
-      self.skipWaiting()
-      resolve()
-    }, 1000)
+self.addEventListener("install", (event) => {
+  const cacheAppShell = caches.open("cache-1").then((cache) => {
+    return cache.addAll([
+      "/",
+      "/index.html",
+      "/js/app.js",
+      "/sw.js",
+      "/static/js/bundle.js",
+      "/favicon.ico",
+    ])
   })
-  event.waitUntil(instalacion)
+  event.waitUntil(cacheAppShell)
 })
 
-self.addEventListener("activate", function (event) {
-  console.log("Service worker activated")
-})
-
-self.addEventListener("fetch", function (event) {
-  // console.log("Fetch: ", event.request.url)
-  // if (event.request.url.includes("https://fakestoreapi.com/products")) {
-  //   const resp = new Response(`{"ok": false, "message": "Interceptado por sw"}`)
-  //   event.respondWith(resp)
-  // }
-})
-
-// self.addEventListener("sync", function (event) {
-//   console.log("Tenemos conexión")
-//   console.log(event)
-//   console.log(event.tag)
-// })
-
-self.addEventListener("push", (event) => {
-  console.log("push")
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request))
 })
